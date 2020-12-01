55560dd98d8762501b307995b42725739244fccb
Joel Wasserman <joel.wasserman8@gmail.com>
2020-12-01 15:40:12 -0800

    added ruby resolver

---
+++ rubygems/index.js
@@ -0,0 +1,230 @@
+const got = require('got')
+const compareVersions = require('compare-versions')
+const limit = require('call-limit')
+
+class RubyGemsDependencyResolver {
+  constructor({ log }) {
+    this.log = log
+    this.got = limit.promise(got, 30)
+  }
+
+  // a regex-type string list that represents the search pattern
+  // for this language/registry's manifest files
+  getManifestPatterns() {
+    return ["Gemfile"]
+  }
+
+  // Def: given a raw file (i.e. the bytes of Gemfile), return a list ([]) of
+  //  dependencies that are listed in the manifest file. The format of each entry in the list
+  //  should be consumable (one at a time) by getSpec.
+  extractDependenciesFromManifest({ manifest }) {
+
+    // TODO: If we detect that the source of the manifest ISN"T rubygems.org, should we even parse the deps??
+
+    // TODO: doesn't support `when '1.9.1'; gem 'ruby-debug-base19', '0.11.23'` syntax
+
+    if (!manifest) return []
+    const extract = manifest.split("\n").reduce((acc, line) => {
+      /**
+       * trim off leading and trailing whitespace
+       * and lowercase the requirement to avoid duplication
+       */
+      line = line.trim().toLowerCase()
+      /**
+       * skip all blank lines
+       */
+      if (line === "") return acc
+      /**
+       * skip all lines that are comments
+       */
+      if (line.startsWith("#")) return acc
+      /**
+       * Add all lines that start with "gem" since we're including deps from every group
+       */
+      if (line.startsWith("gem")) {
+        // Split on # and remove the comments at the end of any lines
+        const lineWithoutComment = line.split('#')[0]
+        return acc.concat(lineWithoutComment)
+      }
+      return acc
+    }, [])
+    return extract
+  }
+
+  /**
+   * - input: a single entry from extractDependenciesFromManifest, input format is of 
+   * "gem <name of gem>, "version (optional)", specifiers (optional)
+   */
+  getSpec(pkg) {
+    if (typeof pkg === "object") return pkg
+
+    // Version seems to always come after the gem name, if it is specified. 
+    const pkgParts = pkg.split(",")
+    // remove gem, trim off white space, remove quotes, now we have the name
+    const name = pkgParts[0].replace('gem', '').trim().replace(/'/g, '').replace(/"/g, '')
+
+    // If there's nothing after the name, then return latest as version
+    if (!pkgParts[1]) {
+      return {
+        name,
+        operator: '==',
+        versionSpec: 'latest',
+        toString: () => `${name}@`
+      }
+    }
+
+    const re = /^('|")(==|=|>=|>|<=|~>)?\s*([0-9.]+)('|")$/
+    const match = pkgParts[1].trim().match(re)
+    if (!match) throw new Error(`unparseable pkg dependency ${pkgParts[1]}`)
+    const operator = match[2] || '='
+    const version = match[3]
+
+    return {
+      name,
+      operator,
+      versionSpec: version,
+      toString: () => `${name}@${operator}${version}`
+    }
+  }
+
+  // To list versions - hit https://rubygems.org/api/v1/versions/[gem name].json
+  // To get deps of specific version - hit https://rubygems.org/api/v2/rubygems/[gem name]/versions/[version].json
+  // to get latest version deps - hit https://rubygems.org/api/v2/rubygems/[gem name].json
+  async getDependencies(pkgSpec) {
+    let name
+    let version
+
+    try {
+      const resolved = await this.resolve(pkgSpec)
+      name = resolved.name
+      version = resolved.version
+    } catch (e) {
+      this.log.error(e)
+      // unable to resolve the given spec; no way to get the deps for this input
+      return []
+    }
+
+    let body
+    if (!version) {
+      ({ body } = await this.got(`https://rubygems.org/api/v2/rubygems/${name}.json`, {
+        responseType: "json"
+      }))
+    } else {
+      ({ body } = await this.got(
+        `https://rubygems.org/api/v2/rubygems/${name}/versions/${version}.json`,
+        {
+          responseType: "json"
+        }
+      ))
+    }
+
+    const dependencyKeys = Object.keys(body.dependencies)
+    const depRequirements = dependencyKeys.reduce((acc, key) => {
+      const runtime = body.dependencies[key]
+      return acc.concat(runtime.reduce((ac, dep) => {
+        // each dep is formatted like this
+        /**
+         * {
+              "name": "activerecord",
+              "requirements": "= 3.0.18"
+          }
+        */
+        const versionSplit = dep.requirements.split(' ')
+        const spec = {
+          name: dep.name,
+          version: versionSplit[1],
+          operator: versionSplit[0],
+          toString: () => `${dep.name}@${versionSplit[0]}${versionSplit[1]}`
+        }
+        return ac.concat(spec)
+      }, []))
+    }, [])
+
+    if (!depRequirements || !depRequirements.length) return []
+
+    return depRequirements
+  }
+
+  // input: output of getSpec
+  // resolves the most suitable version to freeze given <name><operator><version>
+  // for example: rubocop >= 3.0.0 would fetch all versions available, and select the highest version
+  async resolve(pkgSpec) {
+    const { name, operator, versionSpec: version } = pkgSpec
+
+    if (operator === "==" || operator === '=') {
+      return { name, version }
+    }
+
+    // Fetch all tags for a package from https://rubygems.org/api/v1/versions/[gem name].json . 
+    // response will be an array of releases with a "number" key
+    const { body } = (await this.got(`https://rubygems.org/api/v1/versions/${name}.json`,
+      {
+        responseType: "json"
+      }
+    ))
+    // Grab releases and sort them greatest to least
+    const releases = body.map((rel) => rel.number)
+      .sort(compareVersions)
+      .reverse()
+
+    if (!releases.length) throw new Error("No releases found")
+
+    // If version is latest, return the first element of the releases array, representing the latest release
+    if (version === "latest") {
+      return { name, version: releases.shift() }
+    }
+
+    let release
+
+    // Find the highest version out of the tags that satisfy the requirements
+    switch (operator) {
+      case ">=":
+      case "<=":
+      case ">":
+        release = releases.find(rel =>
+          compareVersions.compare(rel, version, operator)
+        )
+        break
+      case "~>":
+        // Fetch latest version under the next specified minor
+        const versionComponents = version.split('.')
+        // If length of the version components is 2, then means fetch up until the next major
+        // if length of the version components is 3, then it means fetch up to the next minor
+        let nextVersion
+        if (versionComponents.length === 2) {
+          nextVersion = `${parseInt(versionComponents[0])+1}.${versionComponents[1]}`
+        } else {
+          nextVersion = `${versionComponents[0]}.${parseInt(versionComponents[1])+1}.${versionComponents[2]}`
+        }
+        release = releases.find(rel =>
+          compareVersions.compare(rel, nextVersion, '<')
+        )
+        break
+      default:
+        throw new Error("Unable to parse version")
+    }
+
+    if (!release) {
+      throw new Error("no version release that satisfies requirements")
+    }
+
+    return { name, version: release }
+  }
+
+  /**
+   * - input: same as getSpec -- a "manifest-style" package specifier (e.g. sodium-native@^1.1.1)
+   *- output: a "locked" package version string -- with no ambiguity (e.g. sodium-native@1.4.0)
+   * this is done by calling the registry 
+   */
+  async resolveToSpec(pkg) {
+    try {
+      const spec = this.getSpec(pkg)
+      const { name, version } = this.resolve(spec)
+      return `${name}==${version}`
+    } catch {}
+    // Fall back to just returning the input
+    return pkg
+  }
+}
+
+module.exports = RubyGemsDependencyResolver
---
+++ rubygems/test/index.test.js
@@ -0,0 +1,218 @@
+const test = require('ava')
+const sinon = require('sinon')
+const limit = require('call-limit')
+const RubyGemsDependencyResolver = require('../')
+
+test.before((t) => {
+  sinon.stub(limit, 'promise')
+})
+
+test.beforeEach((t) => {
+  t.context.log = { warn: sinon.stub(), error: sinon.stub() }
+  t.context.rubygems = new RubyGemsDependencyResolver({ log: t.context.log })
+  t.context.rubygems.got = sinon.stub()
+})
+
+test.after(() => {
+  limit.promise.restore()
+})
+
+test('getManifestPatterns', (t) => {
+  t.deepEqual(t.context.rubygems.getManifestPatterns(), ['Gemfile'])
+})
+
+test('extractDependenciesFromManifest', (t) => {
+  const { rubygems } = t.context
+  const manifest = `
+  gem 'sqlite3'
+  # Use Puma as the app server
+  gem 'puma', '~> 3.7'
+  # Use SCSS for stylesheets
+  gem 'sass-rails', '~> 5.0'
+  `
+
+  const deps = rubygems.extractDependenciesFromManifest({ manifest })
+
+  t.deepEqual(deps, [
+    "gem 'sqlite3'",
+    "gem 'puma', '~> 3.7'",
+    "gem 'sass-rails', '~> 5.0'",
+  ])
+})
+
+test('extractDependenciesFromManifest | bad manifest', (t) => {
+  const { rubygems } = t.context
+  const manifest = ''
+  const deps = rubygems.extractDependenciesFromManifest({ manifest })
+
+  t.deepEqual(deps, [])
+})
+
+test('getSpec | returns just name if name is just passed in', (t) => {
+  const pkg = "gem 'rubocop'"
+  t.deepEqual(t.context.rubygems.getSpec(pkg).toString(), 'rubocop@')
+})
+
+test('getSpec | should parse out operator and version from pkg req input', (t) => {
+  const pkg = "gem 'rubocop', '~> 3.7'"
+  t.deepEqual(t.context.rubygems.getSpec(pkg).toString(), 'rubocop@~>3.7')
+})
+
+test('getDependencies | returns empty dependencies of pkg from registry', async (t) => {
+  t.context.rubygems.resolve = sinon.stub().resolves({
+    name: 'vscodium',
+    verson: '1.0.0'
+  })
+  t.context.rubygems.got.returns({
+    body: {
+      dependencies: {
+        development: [],
+        runtime: []
+      }
+    }
+  })
+  const pkg = t.context.rubygems.getSpec("gem 'rubocop'")
+  const deps = await t.context.rubygems.getDependencies(pkg)
+  t.deepEqual(deps, [])
+})
+
+test('getDependencies | returns empty dependencies if resolve throws', async (t) => {
+  t.context.rubygems.resolve = sinon.stub().rejects('error')
+  const pkg = t.context.rubygems.getSpec("gem 'rubocop'")
+  const deps = await t.context.rubygems.getDependencies(pkg)
+  t.deepEqual(deps, [])
+})
+
+test('getDependencies | returns dependencies of pkg from registry', async (t) => {
+  t.context.rubygems.resolve = sinon.stub().resolves({
+    name: 'vscodium',
+    verson: '1.0.0'
+  })
+  t.context.rubygems.got.returns({
+    body: {
+      dependencies: {
+        development: [],
+        runtime: [
+          {
+            "name": "actionmailer",
+            "requirements": "= 3.0.18"
+          }
+        ]
+      }
+    }
+  })
+  const pkg = t.context.rubygems.getSpec("gem 'rubocop', '>= 3.0.0'")
+  const deps = await t.context.rubygems.getDependencies(pkg)
+  t.deepEqual(deps[0].toString(), 'actionmailer@=3.0.18')
+})
+
+test('getDependencies | returns "latests" dependencies of pkg from registry', async (t) => {
+  // When resolve returns undefined version, resolver fetches latest version from rubygems
+  t.context.rubygems.resolve = sinon.stub().resolves({
+    name: 'vscodium',
+    verson: undefined
+  })
+  t.context.rubygems.got.returns({
+    body: {
+      dependencies: {
+        development: [
+          {
+            "name": "actionmailer",
+            "requirements": "= 3.0.18"
+          }
+        ],
+        runtime: []
+      }
+    }
+  })
+  const pkg = t.context.rubygems.getSpec("gem 'rubocop'")
+  const deps = await t.context.rubygems.getDependencies(pkg)
+  t.deepEqual(deps[0].toString(), 'actionmailer@=3.0.18')
+})
+
+test('resolve | return name and version if operator isn\'t there', async (t) => {
+  const pkg = t.context.rubygems.getSpec("gem 'rubocop', '3.1.1'")
+  const res = await t.context.rubygems.resolve(pkg)
+  t.deepEqual(res, {
+    name: 'rubocop',
+    version: '3.1.1'
+  })
+})
+
+test('resolve | return name and version correctly for >=', async (t) => {
+  t.context.rubygems.got.resolves({
+    body: [
+      {
+        number: '3.1.1'
+      }
+    ]
+  })
+  const pkg = t.context.rubygems.getSpec("gem 'rubocop', '>= 3.0.0'")
+  const res = await t.context.rubygems.resolve(pkg)
+  t.deepEqual(res, {
+    name: 'rubocop',
+    version: '3.1.1'
+  })
+})
+
+test('resolve | return name and version correctly for ~> up to next minor', async (t) => {
+  t.context.rubygems.got.returns({
+    body: [
+      {
+        number: '3.1.1'
+      },
+      {
+        number: '2.0.1'
+      },
+      {
+        number: '2.1.1'
+      }
+    ]
+  })
+  const pkg = t.context.rubygems.getSpec("gem 'rubocop', '~> 2.0.0'")
+  const res = await t.context.rubygems.resolve(pkg)
+  t.deepEqual(res, {
+    name: 'rubocop',
+    version: '2.0.1'
+  })
+})
+
+test('resolve | return name and version correctly for ~> up to next major', async (t) => {
+  t.context.rubygems.got.returns({
+    body: [
+      {
+        number: '3.1.1'
+      },
+      {
+        number: '2.9.0'
+      },
+      {
+        number: '2.1.0'
+      }
+    ]
+  })
+  const pkg = t.context.rubygems.getSpec("gem 'rubocop', '~> 2.0'")
+  const res = await t.context.rubygems.resolve(pkg)
+  t.deepEqual(res, {
+    name: 'rubocop',
+    version: '2.9.0'
+  })
+})
+
+test('resolve | throws if no satisfying version found', async (t) => {
+  t.context.rubygems.got.returns({
+    body: [
+      {
+        number: '3.1.1'
+      },
+      {
+        number: '2.9.0'
+      },
+      {
+        number: '2.1.0'
+      }
+    ]
+  })
+  const pkg = t.context.rubygems.getSpec("gem 'rubocop', '>= 4.0.0'")
+  await t.throwsAsync(async () => await t.context.rubygems.resolve(pkg))
+})
